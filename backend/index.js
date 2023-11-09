1868cf587ffeb60cb1743ff46a7ed9f0c2fa12c3
Colton Smith <coltonsmith28@gmail.com>
2023-11-09 14:12:10 -0500

    Connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const mongoDBURL = process.env.mongoDBURL;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT} 🚀`)
+app.get('/', (req, res) => {
+    console.log(res)
+    return res.status(234).send('First route');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT} 🚀`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
